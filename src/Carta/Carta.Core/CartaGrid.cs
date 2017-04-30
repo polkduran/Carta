f8fc08a22a83902b4f6ddd535d9bd1594893548a
polkduran <polkduran@hotmail.com>
2017-05-01 01:48:03 +0200

    Grid composition

--- src/Carta/Carta.Core/CartaGrid.cs
+++ src/Carta/Carta.Core/CartaGrid.cs
@@ -8,26 +8,101 @@ namespace Carta.Core
 {
     public class CartaGrid
     {
+        private Cell[,] _cells;
+
+        public IReadOnlyCollection<CartaLine> Columns { get; private set; }
+
+        public IReadOnlyCollection<CartaLine> Rows { get; private set; }
+
         public CartaGrid(bool[,] grid)
         {
-
+            BuildCellGrid(grid);
         }
 
+        private void BuildCellGrid(bool[,] grid)
+        {
+            _cells = new Cell[grid.GetLength(0), grid.GetLength(1)];
+            var cols = new List<int>[grid.GetLength(0)];
+            var rows = new List<int>[grid.GetLength(1)];
 
-        private Cell[][] _cells;
+            for (int x = 0; x < grid.GetLength(1); x++)
+            {
+                cols[x] = new List<int> { 0 };
+                for (int y = 0; y < grid.GetLength(0); y++)
+                {
+                    if (x == 0)
+                    {
+                        rows[y] = new List<int> { 0 };
+                    }
+                    var filled = grid[x, y];
+                    FillLine(filled, cols[x]);
+                    FillLine(filled, rows[y]);
+                    _cells[x, y] = new Cell(x, y, filled);
+
+                    if (x == grid.GetLength(1) - 1)
+                    {
+                        RemoveLastIfNeeded(rows[y]);
+                    }
+                }
+                RemoveLastIfNeeded(cols[x]);
+            }
+        }
+
+        private void RemoveLastIfNeeded(List<int> blocks)
+        {
+            var lastIndex = blocks.Count - 1;
+            if (lastIndex > 0 && blocks[lastIndex] == 0)
+            {
+                blocks.RemoveAt(lastIndex);
+            }
+        }
+
+        private void FillLine(bool filled, List<int> blocks)
+        {
+            if (filled)
+            {
+                blocks[blocks.Count - 1]++;
+            }
+            else
+            {
+                if (blocks.Count > 1)
+                {
+                    blocks.Add(0);
+                }
+            }
+        }
+
         public Cell GetCell(int x, int y)
         {
-            return _cells[x][y];
+            return _cells[x, y];
         }
-
     }
 
-    public class Cell
+    public class Cell : ObservableObject
     {
         public int X { get; }
         public int Y { get; }
         public bool Filled { get; }
-        public CellVisualState VisualState { get; set; }
+
+        private CellVisualState _visualState;
+        public CellVisualState VisualState
+        {
+            get
+            {
+                return _visualState;
+            }
+            set
+            {
+                Set(ref _visualState, value);
+            }
+        }
+
+        public Cell(int x, int y, bool filled)
+        {
+            X = x;
+            Y = y;
+            Filled = filled;
+        }
     }
 
     public enum CellVisualState
@@ -39,10 +114,14 @@ namespace Carta.Core
 
     public class CartaLine
     {
-        public int Index { get; set; }
-        public IReadOnlyList<int> Blocks { get; set; }
+        public int Index { get; }
+        public IReadOnlyList<int> Blocks { get; }
         public bool Completed { get; set; }
+
+        public CartaLine(int index, IReadOnlyList<int> blocks)
+        {
+            Index = index;
+            Blocks = blocks;
+        }
     }
-
-
 }
---
+++ src/Carta/Carta.Core/ObservableObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Carta.Core
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected bool Set<T>(ref T old, T current, [CallerMemberName] string propertyName = null)
+        {
+            if(EqualityComparer<T>.Default.Equals(old, current))
+            {
+                return true;
+            }
+            old = current;
+            RaisePropertyChanged(propertyName);
+            return true;
+        }
+    }
+}
