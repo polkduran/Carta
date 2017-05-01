30510d37940d9aef7dd6e880273dbc9396acaa04
polkduran <polkduran@hotmail.com>
2017-05-01 02:14:33 +0200

    UT project

---
+++ src/Carta/Carta.Tests/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Carta.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var grid = new bool[3, 3] {
+                { false, true, false },
+                { true, true, false },
+                { false, true, false },
+            };
+
+            var cartaGrid = new Carta.Core.CartaGrid(grid);
+            Assert.IsNotNull(cartaGrid);
+        }
+    }
+}
